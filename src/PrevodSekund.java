97b97a6d06be3a54064dd2371999a2ed319db5c9
vaclavkozeny <vackoze019@pslib.cz>
2023-09-27 13:13:38 +0200

    Dokončeno UsekovaRychlost

--- src/PrevodSekund.java
+++ src/PrevodSekund.java
@@ -1,19 +1,15 @@
 package src;
-
 /*
-Do balíku přidejte třídu (s metodou main) nazvanou PrevodSekund. V metodě main postupně realizujte zadání úloh 1 a 2 z Úlohy02. Program má nejprve načíst tří celá čísla jako počet hodin, minut a sekund a vše převést na celkový počet sekund a vypsat uživateli. Poté program pokračuje dále načte od uživatele jediné číslo představující celkový počet sekund a vypíše časový údaj ve tvaru HH:MM:SS. Pro výpisy použijte metodu format() standardního výstupu.
-Do balíku přidejte třídu UsekovaRychlost. V této třídě (v metodě main) realizujte řešení úlohy 9 z Úlohy02.
-Do balíku přidejte třídu UroceniVkladu. Zde realizujte řešení úlohy 12 z Úlohy02.
- */
+1: Zapište program, který načte počet hodin, minut a sekund (čas závodníka) a převede tyto údaje na jedinou hodnotu v sekundách
+2: Zapište program, který pro zadanou hodnotu v sekundách poskytne odpovídající počet hodin, minut a sekund a výsledné údaje vypíše ve tvaru HH:MM:SS
+*/
+
 
 import java.util.Scanner;
 
 public class PrevodSekund {
 
     public static void main(String[] args) {
-        /*
-        Zapište program, který načte počet hodin, minut a sekund (čas závodníka) a převede tyto údaje na jedinou hodnotu v sekundách
-         */
         int Hours, Minutes, Seconds, Out;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Zadej cas ve formatu [HH MM SS]");
@@ -24,9 +20,6 @@ public class PrevodSekund {
         Out = (Hours*3600) + (Minutes * 60) + Seconds;
         System.out.println("Cas zavodnika je: " + Out + " sekund" );
         //--------------------------------------------------------------------------------
-        /*
-        Zapište program, který pro zadanou hodnotu v sekundách poskytne odpovídající počet hodin, minut a sekund a výsledné údaje vypíše ve tvaru HH:MM:SS
-         */
         int In;
         System.out.println("Zadej pocet sekund, ktere se prevedou na cas ve formatu [HH:MM:SS]");
         In = scanner.nextInt();
---
+++ src/UsekovaRychlost.java
@@ -0,0 +1,42 @@
+package src;
+
+import java.util.Scanner;
+
+/*
+Zapište program pro výpočet úsekové rychlosti vozidla (průměrné rychlosti vozidla ve sledovaném úseku).
+Vypisujte jak dosaženou průměrnou rychlost, tak i o kolik byla překročena maximální povolená rychlost v daném
+úseku. Vstupem budou: maximální povolená rychlost v daném úseku, délka úseku a časové údaje (hh mm sec
+set) průjezdu vozidla začátkem a koncem sledovaného úseku.
+ */
+public class UsekovaRychlost {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //inputs
+        int MaxSpeed, Length, HoursStart, MinutesStart, SecondsStart, HoursEnd, MinutesEnd, SecondsEnd;
+
+        System.out.println("zadejte pozadovane udaje: [maximalní povolena rychlost] [delka useku v KM]");
+        MaxSpeed = scanner.nextInt();
+        Length = scanner.nextInt();
+        System.out.println("zadejte cas startu [HH MM SS set]");
+        HoursStart = scanner.nextInt();
+        MinutesStart = scanner.nextInt();
+        SecondsStart = scanner.nextInt();
+        String set1 = scanner.next();
+        System.out.println("zadejte cas konce [HH MM SS set]");
+        HoursEnd = scanner.nextInt();
+        MinutesEnd = scanner.nextInt();
+        SecondsEnd = scanner.nextInt();
+        String set2 = scanner.next();
+        //System.out.println(MinutesEnd);
+        int Hours, Minutes, Seconds, Time;
+        double Speed;
+        Hours = HoursEnd - HoursStart;
+        Minutes = MinutesEnd - MinutesStart;
+        Seconds = SecondsEnd - SecondsStart;
+        Time = (Hours * 3600) + (Minutes * 60) + Seconds;
+        Speed = (((double)Length*1000) / Time)*3.6;
+        Speed = Math.round(Speed);
+        //System.out.println(s);
+        System.out.printf("Vase rychlost byla %f, povolenou rychlost jste prekrocil o %f", Speed, (Speed-MaxSpeed));
+    }
+}
