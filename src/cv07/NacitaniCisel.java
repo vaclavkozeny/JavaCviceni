0acbfc2d2b1b7069bd87d2a8644f769317af4f60
vaclavkozeny <vackoze019@pslib.cz>
2023-11-02 08:57:15 +0100

    hotovo cv07

--- src/cv07/NacitaniCisel.java
+++ src/cv07/NacitaniCisel.java
@@ -7,7 +7,7 @@ public class NacitaniCisel {
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         int soucet = 0;
-        int prumer;
+        float prumer;
         int pocet = 0;
         int a;
 
@@ -19,36 +19,36 @@ public class NacitaniCisel {
             soucet = soucet + a;
             pocet++;
         }while(a > 0);
-        prumer = soucet / pocet;
-        System.out.format("Prumer je %d, soucet je %d",prumer,soucet);
+        prumer = (float)soucet / pocet;
+        System.out.format("Prumer je %.1f, soucet je %d\n",prumer,soucet);
 
         //-------------------------
-//        System.out.println("Zadej pocet cislic");
-//        int s = scanner.nextInt();
-//        System.out.println("Zadej cisla");
-//        float min = scanner.nextFloat();
-//        float max = min;
-//        for(int i = 0; i < s-1; i++){
-//            float n = scanner.nextFloat();
-//            min = Math.min(n,min);
-//            max = Math.max(n,max);
-//        }
-//        System.out.format("Min %.1f, Max %.1f", min, max);
+        System.out.println("Zadej pocet cislic\n");
+        int s = scanner.nextInt();
+        System.out.println("Zadej cisla\n");
+        float min = scanner.nextFloat();
+        float max = min;
+        for(int i = 0; i < s-1; i++){
+            float n = scanner.nextFloat();
+            min = Math.min(n,min);
+            max = Math.max(n,max);
+        }
+        System.out.format("Min %.1f, Max %.1f\n", min, max);
         //-------------------------
-//        System.out.println("Zadej pocet cislic");
-//        int s1 = scanner.nextInt();
-//        System.out.println("Zadej cisla");
-//        float last = scanner.nextFloat();
-//        boolean vysledek = true;
-//        for(int i = 0; i < s1-1; i++){
-//            float n = scanner.nextFloat();
-//            if(last > n){
-//                vysledek = false;
-//                break;
-//            }
-//            last = n;
-//        }
-//        System.out.format("Posloupnost %s", vysledek);
+        System.out.println("Zadej pocet cislic\n");
+        int s1 = scanner.nextInt();
+        System.out.println("Zadej cisla\n");
+        float last = scanner.nextFloat();
+        boolean vysledek = true;
+        for(int i = 0; i < s1-1; i++){
+            float n = scanner.nextFloat();
+            if(last > n){
+                vysledek = false;
+                break;
+            }
+            last = n;
+        }
+        System.out.format("Posloupnost %s\n", vysledek);
 
     }
 }
