40d37123dc5c6484372bf30a870379d84c0a5d76
vaclavkozeny <vackoze019@pslib.cz>
2023-11-05 15:09:54 +0100

    hotovo hadani cisla

--- src/cv08/tools/MathTools.java
+++ src/cv08/tools/MathTools.java
@@ -1,15 +1,46 @@
-//package src.cv08.tools;
-//
-//public final class MathTools {
-//    private MathTools(){}
-//    public static double mocnina(double x, double y, double eps){
-//
-//    }
-//    public static double sin(double x, double eps){
-//
-//    }
+package src.cv08.tools;
+
+public final class MathTools {
+    private MathTools(){}
+    public static double mocnina(double x, double y){
+        if(y < 0){
+            x = 1/x;
+            y = -y;
+        }else if(y == 0){
+            return 1;
+        }
+        double r = x;
+        for(int i = 0; i < y-1; i++){
+            r = r*x;
+        }
+        return r;
+    }
+    public static double sin(double x, double eps){
+
+        double sinX = 0;
+        double aktualniclen = x;
+        int n = 1;
+        //tayloruv rozvoj
+        while (absolute(aktualniclen) > eps) {
+            sinX += aktualniclen;
+            n += 2;
+            aktualniclen = -aktualniclen * x * x / (n * (n - 1));
+        }
+        return sinX;
+    }
 //    public static double log(double x, double eps){
+//        double lnX = 0.0;
+//        double term = x - 1;
 //
+//        for (int i = 1; i <= eps; i++) {
+//            if (i % 2 == 0) {
+//                lnX -= term / i;
+//            } else {
+//                lnX += term / i;
+//            }
+//            term *= (x - 1);
+//        }
+//        return lnX;
 //    }
 //    public static double sqrt(double x, double eps){
 //
@@ -17,4 +48,7 @@
 //    public static double qbrt(double x, double eps){
 //
 //    }
-//}
+    private static double absolute(double x){
+        return x<0?-x:x;
+    }
+}
--- src/cv08/tools/TestMathTool.java
+++ src/cv08/tools/TestMathTool.java
@@ -1,15 +1,20 @@
-//package src.cv08.tools;
-//
-//
-//import java.util.Scanner;
-//
-//public class TestMathTool {
-//    private static final Scanner scanner = new Scanner(System.in);
-//    public static void main(String[] args) {
-//        System.out.println("Zadej x");
-//        double x = scanner.nextDouble();
-//        double eps = scanner.nextDouble();
-//        double sin1 = MathTools.sin(x, eps);
-//        double sin2 = Math.sin(x);
-//    }
-//}
+package src.cv08.tools;
+
+
+import java.util.Scanner;
+
+public class TestMathTool {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Zadej x");
+        double x = scanner.nextDouble();
+//        int y = scanner.nextInt();
+//        double mocnina = MathTools.mocnina(x,y);
+//        System.out.println(mocnina);
+        double eps = scanner.nextDouble();
+        double sin1 = MathTools.sin(x, eps);
+        double sin2 = Math.sin(x);
+        System.out.println(sin1);
+        System.out.println(sin2);
+    }
+}
