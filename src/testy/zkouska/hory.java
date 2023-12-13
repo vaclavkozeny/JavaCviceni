9c7419e64c3f8ce88751611eb58dd8a3f86b0af4
vaclavkozeny <vackoze019@pslib.cz>
2023-12-13 21:15:34 +0100

    asi to funguje

--- src/testy/zkouska/hory.java
+++ src/testy/zkouska/hory.java
@@ -2,15 +2,21 @@ package src.testy.zkouska;
 
 public class hory {
     public static void main(String[] args) {
-        int[][]a = new int[][]{{1,1,1,1,1},{1,1,1,1,1},{1,1,100,1,1},{1,1,1,1,1},{1,1,1,1,1}};
+        int[][]a = new int[][]{{1,1,1,1,1},{1,1,1,1,1},{1,1,100,1,10},{1,1,1,1,1},{1,1,1,1,1}};
         int pocetV = 0;
         for (int i = 2; i < a.length-2; i++) {
-            for (int j = 2; j < a[0].length-1; j++) {
+            for (int j = 2; j < a[0].length-2; j++) {
                 int v = a[i][j];
+                System.out.println(v);
                 boolean jeVrchol = true;
-                for (int k = i-2; k < i+2; k++) {
-                    for (int m = j-2; m < j+2 ; m++) {
-                        if(v-a[k][m] >= 5){
+                int vvalue = v;
+                for (int k = i-2; k < i+3; k++) {
+                    for (int m = j-2; m < j+3 ; m++) {
+                        //System.out.println(a[k][m]);
+                        if(i == k && j == m){
+                            continue;
+                        }
+                        if(v-a[k][m] <= 5){
                             jeVrchol = false;
                         }
                     }
